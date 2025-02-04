87c835847e4b9a79d43e615891226652da3084cf
pkc918 <xiaopan2000918@gmail.com>
2025-02-04 19:22:15 +0800

    feat: added ast for newyear

---
+++ ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import "github.com/newyear/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement 语句
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression 表达式
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program 语法分析器生成的每个AST的根结点
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET词法单元
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
