b2a1c3ff420f79e0ef139df2e1f08b08bc7b254c
pkc918 <xiaopan2000918@gmail.com>
2025-02-04 21:13:09 +0800

    test: added unit test for parser

---
+++ parser/parser_test.go
@@ -0,0 +1,61 @@
+package parser
+
+import (
+	"github.com/newyear/ast"
+	"github.com/newyear/lexer"
+	"testing"
+)
+
+func TestLetStatement(t *testing.T) {
+	input := `
+let x = 5;
+let y = 2;
+let foo = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.parseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foo"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, stmt ast.Statement, name string) bool {
+	if stmt.TokenLiteral() != "let" {
+		t.Errorf("stmt.TokenLiteral got %s, want let", stmt.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := stmt.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("stmt.Statement got %T wanted *ast.LetStatement", stmt)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value got %s, want %s", letStmt.Name.Value, name)
+		return false
+	}
+
+	return true
+}
